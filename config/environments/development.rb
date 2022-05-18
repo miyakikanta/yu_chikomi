9d4eb26706aeba1c15dce395c0434509f7f7d97a
miyakikanta <m68kan@gmail.com>
2022-05-18 01:47:27 +0000

    homesコントローラ作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "1ae523fd3dba4d01ba6c812cbad9db2a.vfs.cloud9.us-east-1.amazonaws.com"
 end
